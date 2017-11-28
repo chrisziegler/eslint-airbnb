8ba79062456e227122dc382f3005de2dbae9e8a7
chrisziegler <chrisrziegler@gmail.com>
2017-11-27 23:52:25 -0800

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const bar = 10;
 const foo = bar + 20;
 
+const myObj = {
+  name: 'Chris',
+  age: 50
+};
+
+const work = new Promise((resolve, reject) => {
+  resolve('Some data');
+});
+work.then((data) => {
+  console.log(data);
+});
+
+myObj.age = 51;
+
 document.write(foo);
